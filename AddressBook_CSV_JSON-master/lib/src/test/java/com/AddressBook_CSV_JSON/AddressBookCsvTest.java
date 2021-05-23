9a8e80a8a87dbbb446007d5886c24d7d8dde4ee2
TANVI SHARMA <82575220+TANVISHARMA08@users.noreply.github.com>
2021-05-23 19:31:15 +0530

    Update AddressBookCsvTest.java

--- AddressBook_CSV_JSON-master/lib/src/test/java/com/AddressBook_CSV_JSON/AddressBookCsvTest.java
+++ AddressBook_CSV_JSON-master/lib/src/test/java/com/AddressBook_CSV_JSON/AddressBookCsvTest.java
@@ -8,7 +8,7 @@ public class AddressBookCsvTest {
 	@Test
 	public void givenThreeContacts_WhenWrittenIn_CSVFileShouldPassTheTest() 
     {
-		AddressBookCSV addressBookCSVobject=new AddressBookCSV("C:\\Users\\Mr_Robot\\Desktop\\Fellowship\\AddressBook_CSV_JSON\\lib\\src\\main\\resources\\AddressBookCSVTesting.csv");
+		AddressBookCSV addressBookCSVobject=new AddressBookCSV("C:\\Users\\Tanvi\\Desktop\\assignment\\AddressBook_CSV_JSON\\lib\\src\\main\\resources\\AddressBookCSVTesting.csv");
 		int readCSVContacts=addressBookCSVobject.readAddressBookFromCSVFile();
 		Assert.assertEquals(3,readCSVContacts);
 	} 
@@ -17,7 +17,7 @@ public class AddressBookCsvTest {
 	    public void givenThreeContacts_WhenWrittenAs_JSONFileShouldPassTheTest()
 	    {
 	    		AddressBookJSON addressBookJSON=new AddressBookJSON();
-	    		int numberOfContacts = addressBookJSON.readAddressBookFromAJSONFile("C:\\Users\\Mr_Robot\\Desktop\\Fellowship\\AddressBook_CSV_JSON\\lib\\src\\main\\resources\\AddressBookJSONTesting.json");
+	    		int numberOfContacts = addressBookJSON.readAddressBookFromAJSONFile("C:\\Users\\Tanvi\\Desktop\\assignment\\AddressBook_CSV_JSON\\lib\\src\\main\\resources\\AddressBookJSONTesting.json");
 	    		Assert.assertEquals(3, numberOfContacts);
 	    }
 }
